016dd5af2f9d488b1b8638ce57a1b434375dd566
applul <ray.houston44@gmail.com>
2019-06-13 06:17:58 +0800

    I've so far figured out how to read the input as well as "stringify" it and console log the output. It seems that ( is stringified as 40 and ) as 41. Still deciding whether or not to continue using those variables or first converting those mistranlations back into their original symbols. Sen is advising

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var master_number = 0;
 
 const fs = require('fs'); 
+
+fs.readFile('./input.txt', (err, data) => {
+    console.log(JSON.stringify(data))
+    if (err) {
+        console.log('Error!!!! Please find out the problem and Fix It ASAP!!!! - Sen')
+    }
+})
