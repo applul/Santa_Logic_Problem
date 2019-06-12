a0a44f527acc73c6d964660b80f9ea1dd0565295
applul <ray.houston44@gmail.com>
2019-06-13 06:23:41 +0800

    Assigned tranlated input to a variable

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ var master_number = 0;
 const fs = require('fs'); 
 
 fs.readFile('./input.txt', (err, data) => {
-    console.log(JSON.stringify(data))
+    const Senko = (JSON.stringify(data))
     if (err) {
-        console.log('Error!!!! Please find out the problem and Fix It ASAP!!!! - Sen')
+        console.log('Error!!!! Please find out the problem and Fix It ASAP!!!!')
     }
+    console.log(Senko);
 })
