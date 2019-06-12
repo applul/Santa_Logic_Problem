52ee15d9e139fc3944d97b61631997f94c9d322d
applul <ray.houston44@gmail.com>
2019-06-13 06:43:35 +0800

    Success! const Holo has successully been assigned to be an array of the input. Parsed Correctly

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const fs = require('fs');
 
 fs.readFile('./input.txt', (err, data) => {
     const Senko = (JSON.stringify(data))
-    // const Holo = Senko.split(", ");
+    const SenkoV2 = Senko.replace('{"type":"Buffer","data":[', "");
+    const SenkoV3 = SenkoV2.replace(']}', "");
+    const Holo = SenkoV3.split(",");
     if (err) {
         console.log('Error!!!! Please find out the problem and Fix It ASAP!!!!')
     }
-    // console.log(Holo)
-    // console.log(Holo.length)
+    // console.log(Senko);
+    // console.log(SenkoV2);
+    // console.log(SenkoV3);
+    console.log(Holo)
+    console.log("Holo length", Holo.length)
+    console.log("SenkoV3 Length", SenkoV3.length)
 })
