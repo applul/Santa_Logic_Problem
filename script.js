0d300c81f8f622a10db6f464c632059017adc009
applul <ray.houston44@gmail.com>
2019-06-13 06:33:40 +0800

    still trying to turn the input into an array. Senko.split is not cutting it. For some reason stringify is adding pesky buffer and arbitrary words in the beginning of the string

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 
 fs.readFile('./input.txt', (err, data) => {
     const Senko = (JSON.stringify(data))
+    // const Holo = Senko.split(", ");
     if (err) {
         console.log('Error!!!! Please find out the problem and Fix It ASAP!!!!')
     }
-    console.log(Senko);
+    // console.log(Holo)
+    // console.log(Holo.length)
 })
